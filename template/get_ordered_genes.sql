831a5af91d4a70988220ef5aac80ac1ec259cf74
Getiria Onsongo <onsongoinc@gmail.com>
2015-06-15 08:10:56 -0500

    Fix bug in get_ordered_genes.sql

--- template/get_ordered_genes.sql
+++ template/get_ordered_genes.sql
@@ -1,10 +1,6 @@
-SELECT capture,A.gene_symbol,type,window_id,cnv_ratio,exon_contig_id,avg_window_coverage
+SELECT DISTINCT B.*
 FROM 
-sample_name_tso_cnv A
-JOIN
-(
-SELECT B1.* FROM
-cnv_sample_name_ordered_genes A1
+cnv_sample_name_ordered_genes A
 JOIN
 (
 SELECT gene_symbol, window_id,cnv_ratio,exon_contig_id,avg_window_coverage FROM cnv_sample_name_over_control_name_joint_cov
@@ -16,7 +12,5 @@ UNION
 SELECT gene_symbol, window_id,cnv_ratio,exon_contig_id,avg_window_coverage FROM cnv_sample_name_over_control_name_joint_cov_amp 
 UNION
 SELECT gene_symbol, window_id,cnv_ratio,exon_contig_id,avg_window_coverage FROM cnv_sample_name_over_control_name_joint_cov_oe_amp 
-) B1
-USING(gene_symbol)
 ) B
 USING(gene_symbol) ORDER BY gene_symbol, window_id;
