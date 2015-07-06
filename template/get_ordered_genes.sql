260ccb2a0961c7631de0bcbe85d022dc3155a711
Getiria Onsongo <onsongoinc@gmail.com>
2015-07-06 16:26:55 -0500

    Fixed bug to correctly flag windows as (true/false)

--- template/get_ordered_genes.sql
+++ template/get_ordered_genes.sql
@@ -1,19 +1,19 @@
 SELECT A2.*, random_forest FROM
-(SELECT * FROM 
+(SELECT gene_symbol, window_id,cnv_ratio,exon_contig_id,avg_window_coverage, max(cnv_called) AS cnv_called FROM 
 (SELECT B.*
 FROM 
 cnv_sample_name_ordered_genes A
 JOIN
 (
-SELECT gene_symbol, window_id,cnv_ratio,exon_contig_id,avg_window_coverage FROM cnv_sample_name_over_control_name_joint_cov
+SELECT gene_symbol, window_id,cnv_ratio,exon_contig_id,avg_window_coverage, 'yes' AS cnv_called FROM cnv_sample_name_over_control_name_joint_cov
 UNION
-SELECT gene_symbol, window_id,cnv_ratio,exon_contig_id,avg_window_coverage FROM cnv_sample_name_over_control_name_joint_cov_oe 
+SELECT gene_symbol, window_id,cnv_ratio,exon_contig_id,avg_window_coverage, 'yes' AS cnv_called FROM cnv_sample_name_over_control_name_joint_cov_oe 
 UNION
-SELECT gene_symbol, window_id,cnv_ratio,exon_contig_id,avg_window_coverage FROM cnv_sample_name_over_control_name_joint_control 
+SELECT gene_symbol, window_id,cnv_ratio,exon_contig_id,avg_window_coverage, 'yes' AS cnv_called FROM cnv_sample_name_over_control_name_joint_control 
 UNION
-SELECT gene_symbol, window_id,cnv_ratio,exon_contig_id,avg_window_coverage FROM cnv_sample_name_over_control_name_joint_cov_amp 
+SELECT gene_symbol, window_id,cnv_ratio,exon_contig_id,avg_window_coverage, 'yes' AS cnv_called FROM cnv_sample_name_over_control_name_joint_cov_amp 
 UNION
-SELECT gene_symbol, window_id,cnv_ratio,exon_contig_id,avg_window_coverage FROM cnv_sample_name_over_control_name_joint_cov_oe_amp 
+SELECT gene_symbol, window_id,cnv_ratio,exon_contig_id,avg_window_coverage, 'yes' AS cnv_called FROM cnv_sample_name_over_control_name_joint_cov_oe_amp 
 ) B
 USING(gene_symbol)
 UNION
@@ -21,9 +21,10 @@ SELECT  B1.*
 FROM
 cnv_sample_name_ordered_genes A1
 JOIN
-(SELECT gene_symbol, window_id,cnv_ratio,exon_contig_id,avg_window_coverage FROM cnv_sample_name_over_control_name_60bp_exon_ref1_med_gene_cov) B1
-USING(gene_symbol)) C) A2
+(SELECT gene_symbol, window_id,cnv_ratio,exon_contig_id,avg_window_coverage, 'no' AS cnv_called FROM cnv_sample_name_over_control_name_60bp_exon_ref1_med_gene_cov) B1
+USING(gene_symbol)) C GROUP BY window_id) A2
 LEFT JOIN
 sample_name_predicted
 USING(window_id)
 ORDER BY gene_symbol, window_id;
+-- NOTE: We are using group by so it can pick up entry from the YES group as opposed to NO. Easy way to distinguish called and uncalled windows
