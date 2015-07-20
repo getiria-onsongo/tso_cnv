074949b40c7d5f813f8796bdcc7a9f2517c5090a
Getiria Onsongo <onsongoinc@gmail.com>
2015-07-20 11:14:15 -0500

    Add gc(); in create_tables_ref.R for garbage collection

--- template/create_tables_ref.R
+++ template/create_tables_ref.R
@@ -20,20 +20,19 @@ res=try(cnv_median_window_coverage(con, sample_table_name, output_table_name));
 if(class(res) == "try-error"){
    quit(save = "no", status = 1, runLast = FALSE)
 }
-
+gc();
 sample_table_name <- "cnv_sample_name_over_control_name_60bp_exon_ref2";
 output_table_name <- "cnv_sample_name_over_control_name_60bp_exon_ref2_med";
 res=try(cnv_median_window_coverage(con, sample_table_name, output_table_name));
 if(class(res) == "try-error"){
    quit(save = "no", status = 1, runLast = FALSE)
 }
-
-
+gc();
 sample_table_name <- "cnv_sample_name_over_control_name_60bp_exon_ref3";
 output_table_name <- "cnv_sample_name_over_control_name_60bp_exon_ref3_med";
 res=try(cnv_median_window_coverage(con, sample_table_name, output_table_name));
 if(class(res) == "try-error"){
    quit(save = "no", status = 1, runLast = FALSE)
 }
-
+gc();
 dbDisconnect(con)
