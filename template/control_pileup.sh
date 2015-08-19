b78af66c26d8c728ce22880b332f48c206d5c6f9
Getiria Onsongo <onsongoinc@gmail.com>
2015-08-19 09:48:38 -0500

    Give sample and control fastq files different symbols

--- template/control_pileup.sh
+++ template/control_pileup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-S1_R1=c_s1r1Fastq
-S1_R2=c_s1r2Fastq
-S2_R1=c_s2r1Fastq
-S2_R2=c_s2r2Fastq
+c_S1_R1=c_s1r1Fastq
+c_S1_R2=c_s1r2Fastq
+c_S2_R1=c_s2r1Fastq
+c_S2_R2=c_s2r2Fastq
 
 # Check to see if fastq files are compressed. If they are
 # uncompress them into the working directory
@@ -12,62 +12,62 @@ S2_R2=c_s2r2Fastq
 # there are instances where files permission are not set properly and user is unable to read files from data release. 
 # This copying is a precautionary measure to make sure the program does not break if that happens. 
 
-if [[ $S1_R1 = *.gz ]] ; then
-    gunzip -c $S1_R1 > S1_R1.fastq
-    S1_R1=S1_R1.fastq
+if [[ $c_S1_R1 = *.gz ]] ; then
+    gunzip -c $c_S1_R1 > c_S1_R1.fastq
+    c_S1_R1=c_S1_R1.fastq
 else
-    cp $S1_R1 S1_R1.fastq
-    S1_R1=S1_R1.fastq
+    cp $c_S1_R1 c_S1_R1.fastq
+    c_S1_R1=c_S1_R1.fastq
 fi	
 
-if [[ $S1_R2 = *.gz ]] ; then
-    gunzip -c $S1_R2 > S1_R2.fastq
-    S1_R2=S1_R2.fastq
+if [[ $c_S1_R2 = *.gz ]] ; then
+    gunzip -c $c_S1_R2 > c_S1_R2.fastq
+    c_S1_R2=c_S1_R2.fastq
 else 
-    cp $S1_R2 S1_R2.fastq
-    S1_R2=S1_R2.fastq
+    cp $c_S1_R2 c_S1_R2.fastq
+    c_S1_R2=c_S1_R2.fastq
 fi
 
-if [[ $S2_R1 = *.gz ]] ; then
-    gunzip -c $S2_R1 > S2_R1.fastq
-    S2_R1=S2_R1.fastq
+if [[ $c_S2_R1 = *.gz ]] ; then
+    gunzip -c $c_S2_R1 > c_S2_R1.fastq
+    c_S2_R1=c_S2_R1.fastq
 else
-    cp $S2_R1 S2_R1.fastq
-    S2_R1=S2_R1.fastq
+    cp $c_S2_R1 c_S2_R1.fastq
+    c_S2_R1=c_S2_R1.fastq
 fi	
 
-if [[ $S2_R2 = *.gz ]] ; then
-    gunzip -c $S2_R2 > S2_R2.fastq
-    S2_R2=S2_R2.fastq
+if [[ $c_S2_R21 = *.gz ]] ; then
+    gunzip -c $c_S2_R21 > c_S2_R2.fastq
+    c_S2_R2=c_S2_R2.fastq
 else
-    cp $S2_R2 S2_R2.fastq
-    S2_R2=S2_R2.fastq
+    cp $c_S2_R21 c_S2_R2.fastq
+    c_S2_R2=c_S2_R2.fastq
 fi   
 
 BWA_DB=bwa_db_value
 BOWTIE2_DB=bowtie2_db_value
 S_DB=seq_db
 
-bwa mem -M -t 24 $BWA_DB $S1_R1 $S1_R2 > bwa_s1.sam
-bwa mem -M -t 24 $BWA_DB $S2_R1 $S2_R2 > bwa_s2.sam
-bowtie2 -p 24 -k 5 -x $BOWTIE2_DB -1 $S1_R1 -2 $S1_R2 -S bowtie2_s1.sam
-bowtie2 -p 24 -k 5 -x $BOWTIE2_DB -1 $S2_R1 -2 $S2_R2 -S bowtie2_s2.sam
+bwa mem -M -t 24 $BWA_DB $c_S1_R1 $c_S1_R2 > c_bwa_s1.sam
+bwa mem -M -t 24 $BWA_DB $c_S2_R1 $c_S2_R21 > c_bwa_s2.sam
+bowtie2 -p 24 -k 5 -x $BOWTIE2_DB -1 $c_S1_R1 -2 $c_S1_R2 -S c_bowtie2_s1.sam
+bowtie2 -p 24 -k 5 -x $BOWTIE2_DB -1 $c_S2_R1 -2 $c_S2_R21 -S c_bowtie2_s2.sam
 
-samtools view -q 10 -bS bwa_s1.sam > bwa_s1.bam
-samtools view -q 10 -bS bwa_s2.sam > bwa_s2.bam
-samtools view -q 10 -bS bowtie2_s1.sam > bowtie2_s1.bam
-samtools view -q 10 -bS bowtie2_s2.sam > bowtie2_s2.bam
+samtools view -q 10 -bS c_bwa_s1.sam > c_bwa_s1.bam
+samtools view -q 10 -bS c_bwa_s2.sam > c_bwa_s2.bam
+samtools view -q 10 -bS c_bowtie2_s1.sam > c_bowtie2_s1.bam
+samtools view -q 10 -bS c_bowtie2_s2.sam > c_bowtie2_s2.bam
 
-samtools merge bwa.bam bwa_s1.bam bwa_s2.bam
-samtools merge bowtie2.bam bowtie2_s1.bam bowtie2_s2.bam
+samtools merge c_bwa.bam c_bwa_s1.bam c_bwa_s2.bam
+samtools merge c_bowtie2.bam c_bowtie2_s1.bam c_bowtie2_s2.bam
 
-java -Xmx4g -jar  $CLASSPATH/picard.jar FixMateInformation SORT_ORDER=coordinate INPUT=bwa.bam OUTPUT=bwa.fixed.bam
-java -Xmx4g -jar  $CLASSPATH/picard.jar MarkDuplicates REMOVE_DUPLICATES=true ASSUME_SORTED=true METRICS_FILE=bwa_duplicate_stats.txt INPUT=bwa.fixed.bam OUTPUT=bwa.fixed_nodup.bam
-java -Xmx4g -jar  $CLASSPATH/picard.jar FixMateInformation SORT_ORDER=coordinate INPUT=bowtie2.bam OUTPUT=bowtie2.fixed.bam
+java -Xmx4g -jar  $CLASSPATH/picard.jar FixMateInformation SORT_ORDER=coordinate INPUT=c_bwa.bam OUTPUT=c_bwa.fixed.bam
+java -Xmx4g -jar  $CLASSPATH/picard.jar MarkDuplicates REMOVE_DUPLICATES=true ASSUME_SORTED=true METRICS_FILE=c_bwa_duplicate_stats.txt INPUT=c_bwa.fixed.bam OUTPUT=c_bwa.fixed_nodup.bam
+java -Xmx4g -jar  $CLASSPATH/picard.jar FixMateInformation SORT_ORDER=coordinate INPUT=c_bowtie2.bam OUTPUT=c_bowtie2.fixed.bam
 
-samtools mpileup -f $S_DB -d 10000 -q 1 bwa.fixed.bam | cut -f 1,2,4 > cnv_control_name_bwa_pileup.txt
-samtools mpileup -f $S_DB -d 10000 -q 1 bwa.fixed_nodup.bam | cut -f 1,2,4 > cnv_control_name_bwa_pileup_no_dup.txt
-samtools mpileup -f $S_DB -d 10000 -q 1 bowtie2.fixed.bam | cut -f 1,2,4 > cnv_control_name_bowtie2_pileup.txt
+samtools mpileup -f $S_DB -d 10000 -q 1 c_bwa.fixed.bam | cut -f 1,2,4 > cnv_control_name_bwa_pileup.txt
+samtools mpileup -f $S_DB -d 10000 -q 1 c_bwa.fixed_nodup.bam | cut -f 1,2,4 > cnv_control_name_bwa_pileup_no_dup.txt
+samtools mpileup -f $S_DB -d 10000 -q 1 c_bowtie2.fixed.bam | cut -f 1,2,4 > cnv_control_name_bowtie2_pileup.txt
 
 rm -rf *.bam
 rm -rf *.sam
--- template/sample_pileup.sh
+++ template/sample_pileup.sh
@@ -1,73 +1,73 @@
 #!/bin/bash
 
-S1_R1=s_s1r1Fastq
-S1_R2=s_s1r2Fastq
-S2_R1=s_s2r1Fastq
-S2_R2=s_s2r2Fastq
+s_S1_R1=s_s1r1Fastq
+s_S1_R2=s_s1r2Fastq
+s_S2_R1=s_s2r1Fastq
+s_S2_R2=s_s2r2Fastq
 
-# Check to see if fastq files are compressed. If they are                                                                         
-# uncompress them into the working directory                                                                                      
-#                                                                                                                                 
-# NOTE: The copying in the ELSE clause is not necessary. The files should be readable from data release. However,                 
-# there are instances where files permission are not set properly and user is unable to read files from data release.             
-# This copying is a precautionary measure to make sure the program does not break if that happens.                                
+# Check to see if fastq files are compressed. If they are
+# uncompress them into the working directory
+#
+# NOTE: The copying in the ELSE clause is not necessary. The files should be readable from data release. However, 
+# there are instances where files permission are not set properly and user is unable to read files from data release. 
+# This copying is a precautionary measure to make sure the program does not break if that happens. 
 
-if [[ $S1_R1 = *.gz ]] ; then
-    gunzip -c $S1_R1 > S1_R1.fastq
-    S1_R1=S1_R1.fastq
+if [[ $s_S1_R1 = *.gz ]] ; then
+    gunzip -c $s_S1_R1 > s_S1_R1.fastq
+    s_S1_R1=s_S1_R1.fastq
 else
-    cp $S1_R1 S1_R1.fastq
-    S1_R1=S1_R1.fastq
+    cp $s_S1_R1 s_S1_R1.fastq
+    s_S1_R1=s_S1_R1.fastq
+fi	
+
+if [[ $s_S1_R2 = *.gz ]] ; then
+    gunzip -c $s_S1_R2 > s_S1_R2.fastq
+    s_S1_R2=s_S1_R2.fastq
+else 
+    cp $s_S1_R2 s_S1_R2.fastq
+    s_S1_R2=s_S1_R2.fastq
 fi
 
-if [[ $S1_R2 = *.gz ]] ; then
-    gunzip -c $S1_R2 > S1_R2.fastq
-    S1_R2=S1_R2.fastq
+if [[ $s_S2_R1 = *.gz ]] ; then
+    gunzip -c $s_S2_R1 > s_S2_R1.fastq
+    s_S2_R1=s_S2_R1.fastq
 else
-    cp $S1_R2 S1_R2.fastq
-    S1_R2=S1_R2.fastq
-fi
+    cp $s_S2_R1 s_S2_R1.fastq
+    s_S2_R1=s_S2_R1.fastq
+fi	
 
-if [[ $S2_R1 = *.gz ]] ; then
-    gunzip -c $S2_R1 > S2_R1.fastq
-    S2_R1=S2_R1.fastq
+if [[ $s_S2_R21 = *.gz ]] ; then
+    gunzip -c $s_S2_R21 > s_S2_R2.fastq
+    s_S2_R2=s_S2_R2.fastq
 else
-    cp $S2_R1 S2_R1.fastq
-    S2_R1=S2_R1.fastq
-fi
-
-if [[ $S2_R2 = *.gz ]] ; then
-    gunzip -c $S2_R2 > S2_R2.fastq
-    S2_R2=S2_R2.fastq
-else
-    cp $S2_R2 S2_R2.fastq
-    S2_R2=S2_R2.fastq
-fi
+    cp $s_S2_R21 s_S2_R2.fastq
+    s_S2_R2=s_S2_R2.fastq
+fi   
 
 BWA_DB=bwa_db_value
 BOWTIE2_DB=bowtie2_db_value
 S_DB=seq_db
 
-bwa mem -M -t 24 $BWA_DB $S1_R1 $S1_R2 > bwa_s1.sam
-bwa mem -M -t 24 $BWA_DB $S2_R1 $S2_R2 > bwa_s2.sam
-bowtie2 -p 24 -k 5 -x $BOWTIE2_DB -1 $S1_R1 -2 $S1_R2 -S bowtie2_s1.sam
-bowtie2 -p 24 -k 5 -x $BOWTIE2_DB -1 $S2_R1 -2 $S2_R2 -S bowtie2_s2.sam
+bwa mem -M -t 24 $BWA_DB $s_S1_R1 $s_S1_R2 > s_bwa_s1.sam
+bwa mem -M -t 24 $BWA_DB $s_S2_R1 $s_S2_R21 > s_bwa_s2.sam
+bowtie2 -p 24 -k 5 -x $BOWTIE2_DB -1 $s_S1_R1 -2 $s_S1_R2 -S s_bowtie2_s1.sam
+bowtie2 -p 24 -k 5 -x $BOWTIE2_DB -1 $s_S2_R1 -2 $s_S2_R21 -S s_bowtie2_s2.sam
 
-samtools view -q 10 -bS bwa_s1.sam > bwa_s1.bam
-samtools view -q 10 -bS bwa_s2.sam > bwa_s2.bam
-samtools view -q 10 -bS bowtie2_s1.sam > bowtie2_s1.bam
-samtools view -q 10 -bS bowtie2_s2.sam > bowtie2_s2.bam
+samtools view -q 10 -bS s_bwa_s1.sam > s_bwa_s1.bam
+samtools view -q 10 -bS s_bwa_s2.sam > s_bwa_s2.bam
+samtools view -q 10 -bS s_bowtie2_s1.sam > s_bowtie2_s1.bam
+samtools view -q 10 -bS s_bowtie2_s2.sam > s_bowtie2_s2.bam
 
-samtools merge bwa.bam bwa_s1.bam bwa_s2.bam
-samtools merge bowtie2.bam bowtie2_s1.bam bowtie2_s2.bam
+samtools merge s_bwa.bam s_bwa_s1.bam s_bwa_s2.bam
+samtools merge s_bowtie2.bam s_bowtie2_s1.bam s_bowtie2_s2.bam
 
-java -Xmx4g -jar  $CLASSPATH/picard.jar FixMateInformation SORT_ORDER=coordinate INPUT=bwa.bam OUTPUT=bwa.fixed.bam
-java -Xmx4g -jar  $CLASSPATH/picard.jar MarkDuplicates REMOVE_DUPLICATES=true ASSUME_SORTED=true METRICS_FILE=bwa_duplicate_stats.txt INPUT=bwa.fixed.bam OUTPUT=bwa.fixed_nodup.bam
-java -Xmx4g -jar  $CLASSPATH/picard.jar FixMateInformation SORT_ORDER=coordinate INPUT=bowtie2.bam OUTPUT=bowtie2.fixed.bam
+java -Xmx4g -jar  $CLASSPATH/picard.jar FixMateInformation SORT_ORDER=coordinate INPUT=s_bwa.bam OUTPUT=s_bwa.fixed.bam
+java -Xmx4g -jar  $CLASSPATH/picard.jar MarkDuplicates REMOVE_DUPLICATES=true ASSUME_SORTED=true METRICS_FILE=s_bwa_duplicate_stats.txt INPUT=s_bwa.fixed.bam OUTPUT=s_bwa.fixed_nodup.bam
+java -Xmx4g -jar  $CLASSPATH/picard.jar FixMateInformation SORT_ORDER=coordinate INPUT=s_bowtie2.bam OUTPUT=s_bowtie2.fixed.bam
 
-samtools mpileup -f $S_DB -d 10000 -q 1 bwa.fixed.bam | cut -f 1,2,4 > cnv_sample_name_bwa_pileup.txt
-samtools mpileup -f $S_DB -d 10000 -q 1 bwa.fixed_nodup.bam | cut -f 1,2,4 > cnv_sample_name_bwa_pileup_no_dup.txt
-samtools mpileup -f $S_DB -d 10000 -q 1 bowtie2.fixed.bam | cut -f 1,2,4 > cnv_sample_name_bowtie2_pileup.txt
+samtools mpileup -f $S_DB -d 10000 -q 1 s_bwa.fixed.bam | cut -f 1,2,4 > cnv_sample_name_bwa_pileup.txt
+samtools mpileup -f $S_DB -d 10000 -q 1 s_bwa.fixed_nodup.bam | cut -f 1,2,4 > cnv_sample_name_bwa_pileup_no_dup.txt
+samtools mpileup -f $S_DB -d 10000 -q 1 s_bowtie2.fixed.bam | cut -f 1,2,4 > cnv_sample_name_bowtie2_pileup.txt
 
 rm -rf *.bam
 rm -rf *.sam
