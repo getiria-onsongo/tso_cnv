65fe34c08629e8c2370d3d7c21a325e290126a82
Getiria Onsongo <onsongoinc@gmail.com>
2015-12-17 14:47:42 -0600

    Updated get_predicted to store raw values

--- template/get_predicted.sql
+++ template/get_predicted.sql
@@ -1,9 +1,14 @@
 DROP TABLE IF EXISTS sample_name_predicted;
 CREATE TABLE sample_name_predicted AS
-SELECT DISTINCT gene_symbol, A.*, 'control_name' AS control
+SELECT DISTINCT A1.*, gene_symbol FROM 
+(SELECT DISTINCT A.window_id,A.mfe,A.gc,A.num_repeats,A.bb_sd,A.cnv_ratio_sd,A.cnv_ratio_dip_stat,A.cov_sd,A.cov_avg,
+A.dup_rat_avg,A.true_deletion,random_forest,A.sample,A.data_type 
 FROM
-sample_name_prediction A
+sample_name_data A
 JOIN
-tso_exon_60bp_segments_window_data B
+sample_name_prediction B
+USING(window_id)) A1
+JOIN
+tso_exon_60bp_segments_window_data B1
 USING(window_id);
 SELECT * FROM sample_name_predicted;
