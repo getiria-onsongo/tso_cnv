f146265a3995c3ce3aa36c8d9f7b765524c2ff1b
Getiria Onsongo <onsongoinc@gmail.com>
2015-12-22 13:20:41 -0600

    Add columns on data output for ordered genes

--- template/get_ordered_genes.sql
+++ template/get_ordered_genes.sql
@@ -1,27 +1,25 @@
-SELECT B4.* FROM 
-cnv_sample_name_ordered_genes A4
-JOIN
-(SELECT gene_symbol,ref_exon_contig_id,A3.window_id,min_bowtie_bwa_ratio,max_bowtie_bwa_ratio,cnv_ratio,exon_contig_id,exon_number,avg_window_coverage,window_number, cnv_called, random_forest
+SELECT DISTINCT A4.*, random_forest AS random_forest_het_del FROM
+(SELECT A3.*, cnv_called FROM
+(SELECT A1.*, B1.avg_window_coverage AS avg_window_cov_control_name, B1.min_bowtie_bwa_ratio AS min_bb_ratio_control_name, B1.max_bowtie_bwa_ratio AS max_bb_ratio_control_name
 FROM
-(SELECT DISTINCT window_id FROM tso_exon_60bp_segments_window_data) A3
-LEFT JOIN
-(SELECT A2.*,random_forest
+(SELECT 
+A.gene_symbol,A.ref_exon_contig_id, A.exon_contig_id, A.exon_number, A.window_id, A.window_number, A.cnv_ratio, 
+A.avg_window_coverage AS avg_window_cov_10_16763, min_bowtie_bwa_ratio AS min_bb_ratio_sample_name, A.max_bowtie_bwa_ratio AS max_bb_ratio_sample_name
 FROM
-(SELECT A1.*, cnv_called FROM
-(SELECT DISTINCT A.gene_symbol,ref_exon_contig_id,window_id,min_bowtie_bwa_ratio,max_bowtie_bwa_ratio,cnv_ratio,exon_contig_id,exon_number,avg_window_coverage,window_number
-FROM cnv_sample_name_over_control_name_60bp_exon_ref1_med_gene_cov A
+cnv_sample_name_over_control_name_60bp_exon_ref1_med_gene_cov A
 JOIN
-cnv_sample_name_ordered_genes
+cnv_sample_name_ordered_genes B
 USING(gene_symbol)) A1
+JOIN
+cnv_sample_name_over_control_name_60bp_exon_ref1_control B1
+USING(window_id)) A3
 LEFT JOIN
 (SELECT window_id,'yes' AS cnv_called FROM cnv_sample_name_over_control_name_joint_cov
 UNION
 SELECT window_id,'yes' AS cnv_called FROM cnv_sample_name_over_control_name_joint_control
 UNION
-SELECT window_id,'yes' AS cnv_called FROM cnv_sample_name_over_control_name_joint_cov_amp) B1
-USING(window_id)) A2
+SELECT window_id,'yes' AS cnv_called FROM cnv_sample_name_over_control_name_joint_cov_amp) B3
+USING(window_id)) A4
 LEFT JOIN
-sample_name_predicted B2
-USING(window_id)) B3
-USING(window_id)) B4
-USING(gene_symbol);
+sample_name_predicted B4
+USING(window_id) ORDER BY window_id;
