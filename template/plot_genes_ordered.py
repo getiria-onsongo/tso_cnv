a4efbf153aa0f4df6633a72afd5bb10edcf91912
Evan Bollig <bollig@gmail.com>
2016-06-21 02:28:38 -0500

    updated plot_genes_ordered.py template for improved plotting efficiency and compression

--- template/plot_genes_ordered.py
+++ template/plot_genes_ordered.py
@@ -3,23 +3,29 @@
 import os
 import itertools
 import matplotlib 
-matplotlib.use('Agg')
+matplotlib.use('PDF')
 import matplotlib.pyplot as plt
-import mysql.connector
-from mysql.connector.errors import Error
+import MySQLdb
+import sys
 
 def cnv_plot_all_bowtie_bwa(con, input_table, pos, filter_column1, filter_value1, data_column1, bowtie_bwa_ratio, y_limit, title_label, dir_path):
 	colors = itertools.cycle(['black','red','green','blue','cyan','magenta','yellow','gray'])
-	image_name = "%s.png" % title_label
+	image_name = "%s.pdf" % title_label
 	os.chdir(dir_path)
-	fig = plt.figure(1, figsize=(23, 6), dpi=600)
+    plt.gcf().clear()
+    plt.gca().clear()
+    for txt in plt.gcf().texts:
+        txt.set_visible(False)
+        txt.remove()
+	fig = plt.figure(figsize=(23, 6)) #, dpi=600)
 
 	get_ref_exon_contig_id = "SELECT DISTINCT ref_exon_contig_id FROM `%s` WHERE %s='%s';"
-	#print(get_ref_exon_contig_id % (input_table, filter_column1, filter_value1))
+	print('Plotting: %s' % image_name)
 
 	cursor = con.cursor()
 	try: 
-		status = cursor.execute(get_ref_exon_contig_id % (input_table, filter_column1, filter_value1))
+		cursor.execute(get_ref_exon_contig_id % (input_table, filter_column1, filter_value1))
+        
 		i_ref_exon = cursor.fetchall()
 		if not i_ref_exon:
 			print("The query[ %s ] returns 0 rows" % get_ref_exon_contig_id)
@@ -32,7 +38,6 @@ def cnv_plot_all_bowtie_bwa(con, input_table, pos, filter_column1, filter_value1
 			status2 = cursor.execute(get_data % (pos, data_column1, bowtie_bwa_ratio, input_table, filter_column1, filter_value1, filter_value2, pos))
 			i_data = cursor.fetchall()
 			if not i_data: 
-				#print("NO DATA FOUND")
 				plt.plot(0, 0.07, 'o', mfc='none', mec='b')
 				plt.text(-0.95, 0.5, 'This gene has no coverage data')
 				plt.xlim(-1, 1)
@@ -64,37 +69,45 @@ def cnv_plot_all_bowtie_bwa(con, input_table, pos, filter_column1, filter_value1
 					plt.axhline(y=1, color='b', ls='dashed')
 					plt.text(0, 0, num_exon, size='6')
 				altPlot = 1
+				i_values = range(0,n)
+				data_value1 = [ x[0] for x in i_data ]
+				data_value2 = [ x[1] for x in i_data ]
+				data_value3 = [ x[2] for x in i_data ]
+				plt.plot(i_values, data_value2, '+', mec=colour, mfc='none')
+				plt.plot(i_values, data_value3, '*', mec='red', mfc='none')
 				for i in range(1,n):
-					data_value1 = i_data[i][0]
-					data_value2 = i_data[i][1]
-					data_value3 = i_data[i][2]
-					plt.plot(i, data_value2, '+', mec=colour, mfc='none')
-					plt.plot(i, data_value3, '*', mec='red', mfc='red')
-					extra=0.05*((n+1)/2)
-					plt.xlim(-1-extra, n+extra)
-					plt.ylim(-0.3, y_limit+0.3) 
 					if (i_data[i][0] - i_data[i-1][0]) > 1:
 						num_exon = num_exon + 1
 						plt.text(i, 0, num_exon,size='6')
 						if altPlot == 1: 
-							plt.text(i, -0.1, data_value1,size='6')
+							plt.text(i, -0.1, i_data[i][0],size='6')
 							altPlot = -1
 						else: 
-							plt.text(i, -0.2, data_value1,size='6')
+							plt.text(i, -0.2, i_data[i][0],size='6')
 							altPlot = 1
 						plt.plot(i, 0.07, 'o', mfc='none', mec='b', linestyle='None')
-			j=j+1
-	except mysql.connector.Error as e: 
+				extra=0.05*((n+1)/2)
+				plt.xlim(-1-extra, n+extra)
+				plt.ylim(-0.3, y_limit+0.3) 
+
+	except MySQLdb.Error as e: 
 		print("Error code:", e.errno)        # error number
   		print("SQLSTATE value:", e.sqlstate) # SQLSTATE value
 		print("Error message:", e.msg)       # error message
 		print("Error:", e)                   # errno, sqlstate, msg values
 		s = str(e)
 		print("Error:", s)                   # errno, sqlstate, msg values
-	plt.savefig(image_name)	
-	cursor.close()
 	
-con = mysql.connector.connect(user='root',
+	print("Saving")
+	plt.savefig(image_name, dpi=600, transparent=False)
+	#print("Closing")
+	cursor.close()
+	plt.gcf().clear()
+	plt.close()
+	
+
+
+con = MySQLdb.connect(user='root',
                               host='127.0.0.1',
                               database='cnv',
 			                  unix_socket='socket_path')
