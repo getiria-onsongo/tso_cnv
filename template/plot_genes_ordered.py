f7e8cc1752970539c2bc16147ff86757085ae317
Evan Bollig <bollig@gmail.com>
2016-06-21 14:50:23 -0500

    Update plot_genes_ordered.py

--- template/plot_genes_ordered.py
+++ template/plot_genes_ordered.py
@@ -116,7 +116,7 @@ def cnv_plot_all_bowtie_bwa(con, input_table, pos, filter_column1, filter_value1
 
 con = MySQLdb.connect(user='root',
                               host='127.0.0.1',
-                              database='cnv',
+                              db='cnv',
 			unix_socket='socket_path')
 dir_path = "sample_path";
 os.chdir(dir_path)
